52e6f09d8bb0366ef74d2a003e3e2b1fb022591d
Bryan Cole <brycole@gmail.com>
2020-01-25 16:52:50 +0000

    added controllers, views, some CSS styling

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,35 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktails, only: %i[show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @doses = @cocktail.doses
+    # @ingredients = @cocktail.ingredients
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_cocktails
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :url)
+  end
 end
